603bff9d68514bfe6985f0ba234632c597b7e169
gjulieta <gjuelieta1@gmail.com>
2026-05-17 19:54:32 -0300

    Agrego clase Punto

---
+++ src/punto.py
@@ -0,0 +1,17 @@
+#crear la clase punto#
+class Punto:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def eje_x(self):
+        return self.x
+
+    def eje_y(self):
+        return self.y
+
+    def impresion(self):
+        return f"({self.x}, {self.y})"
+
+    def opuesto(self):
+        return Punto(-self.x, -self.y)    
